c131e815ffb56a4fe5398816f69719948060e98b
Pedro Henrique <pedrohbm554@gmail.com>
2023-08-06 03:25:44 -0300

    Escrito um Haikai de Paulo Leminski

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Amar é um elo");
+Console.WriteLine("entre o azul");
+Console.WriteLine("e o amarelo");
+Console.WriteLine();
+Console.WriteLine("Paulo Leminski");
